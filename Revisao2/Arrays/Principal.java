e28a8f5e78243f4033d918b39db084a926b29c2f
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-08-26 10:05:43 -0300

    java

--- Revisao2/Arrays/Principal.java
+++ Revisao2/Arrays/Principal.java
@@ -1,30 +1,44 @@
 package Revisao2.Arrays;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Principal {
+  
     public static void main(String[] args) {
         
-        ArrayList <Aluno> aluno = new ArrayList<>();
+        // Pegando a quantidade de Itens da lista 
+        Scanner valor = new Scanner(System.in);
+        System.out.print("Digite o número de intes da lista: ");
+        int numItem = valor.nextInt();
+        // System.out.print("A quantidade de intes é: " + numItem);
 
-        Aluno a1 = new Aluno();
-        a1.setName("João");
-        a1.setRa("pq123");
-        a1.curso.setName("Ciência da computação");
-        a1.curso.setSemestre(4);
+        // Criando a Lista Alunos
+        ArrayList <Aluno> aluno = new ArrayList<Aluno>();
 
-        Aluno a2 = new Aluno();
-        a2.setName("Ana");
-        a2.setRa("cf433");
-        a2.curso.setName("Adiministração");
-        a2.curso.setSemestre(2);
+        // Pegando informações dos Alunos 
+        for(int i = 1; i<=numItem; i++){
+            System.out.println(i + "°" + "Aluno");
+            valor.nextLine();
+            System.out.print("Digite o nome do aluno: ");
+            String nameAluno = valor.nextLine();
+            System.out.print("Digite o RA do aluno: ");
+            String raAluno = valor.nextLine();
 
-        for (int i = 0; i < aluno.size(); i++){
+            Aluno alunos = new Aluno();
 
-            Aluno encontrado = aluno.get(i);
-            
-            System.out.println(encontrado.getName());
-            System.out.println(encontrado.getRa());
+            alunos.setName(nameAluno);
+            alunos.setRa(raAluno);
+            aluno.add(alunos);
+
+        }
+
+        System.out.println("------------------------------------------------------------------");
+        
+        // Exibindo a Lista de Alunos
+        System.out.println("LISTA DE ALUNOS");
+        for( Aluno alunos : aluno){
+            System.out.print( "Nome: "+ alunos.getName() + " RA: " + alunos.getRa() + "\n");
         }
     }
 }
