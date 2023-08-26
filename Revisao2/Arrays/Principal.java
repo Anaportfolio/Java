3fd8c5b07c5b075d380a7062b09a9273ba8b6e26
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-08-26 10:08:15 -0300

    java

--- Revisao2/Arrays/Principal.java
+++ Revisao2/Arrays/Principal.java
@@ -7,38 +7,39 @@ public class Principal {
   
     public static void main(String[] args) {
         
-        // Pegando a quantidade de Itens da lista 
-        Scanner valor = new Scanner(System.in);
-        System.out.print("Digite o número de intes da lista: ");
-        int numItem = valor.nextInt();
-        // System.out.print("A quantidade de intes é: " + numItem);
+        try (// Pegando a quantidade de Itens da lista 
+        Scanner valor = new Scanner(System.in)) {
+            System.out.print("Digite o número de intes da lista: ");
+            int numItem = valor.nextInt();
+            // System.out.print("A quantidade de intes é: " + numItem);
 
-        // Criando a Lista Alunos
-        ArrayList <Aluno> aluno = new ArrayList<Aluno>();
+            // Criando a Lista Alunos
+            ArrayList <Aluno> aluno = new ArrayList<Aluno>();
 
-        // Pegando informações dos Alunos 
-        for(int i = 1; i<=numItem; i++){
-            System.out.println(i + "°" + "Aluno");
-            valor.nextLine();
-            System.out.print("Digite o nome do aluno: ");
-            String nameAluno = valor.nextLine();
-            System.out.print("Digite o RA do aluno: ");
-            String raAluno = valor.nextLine();
+            // Pegando informações dos Alunos 
+            for(int i = 1; i<=numItem; i++){
+                System.out.println(i + "°" + "Aluno");
+                valor.nextLine();
+                System.out.print("Digite o nome do aluno: ");
+                String nameAluno = valor.nextLine();
+                System.out.print("Digite o RA do aluno: ");
+                String raAluno = valor.nextLine();
 
-            Aluno alunos = new Aluno();
+                Aluno alunos = new Aluno();
 
-            alunos.setName(nameAluno);
-            alunos.setRa(raAluno);
-            aluno.add(alunos);
+                alunos.setName(nameAluno);
+                alunos.setRa(raAluno);
+                aluno.add(alunos);
 
-        }
+            }
 
-        System.out.println("------------------------------------------------------------------");
-        
-        // Exibindo a Lista de Alunos
-        System.out.println("LISTA DE ALUNOS");
-        for( Aluno alunos : aluno){
-            System.out.print( "Nome: "+ alunos.getName() + " RA: " + alunos.getRa() + "\n");
+            System.out.println("------------------------------------------------------------------");
+            
+            // Exibindo a Lista de Alunos
+            System.out.println("LISTA DE ALUNOS");
+            for( Aluno alunos : aluno){
+                System.out.print( "Nome: "+ alunos.getName() + " RA: " + alunos.getRa() + "\n");
+            }
         }
     }
 }
