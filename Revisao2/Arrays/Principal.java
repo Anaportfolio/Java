576b715ec544120109631ca0f2061d388569f148
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-09-04 09:12:27 -0300

    java

--- Revisao2/Arrays/Principal.java
+++ Revisao2/Arrays/Principal.java
@@ -3,8 +3,10 @@ package Revisao2.Arrays;
 import java.util.ArrayList;
 
 public class Principal {
+    public static void main(String[] args) {
 
-    ArrayList<Aluno> aluno = new ArrayList<Aluno>();
+        ArrayList<Aluno> aluno = new ArrayList<Aluno>();
 
-    Aluno a1 = new Aluno();
+        Aluno a1 = new Aluno();
+    }
 }
--- Revisao2/Fila/Principal.java
+++ Revisao2/Fila/Principal.java
@@ -4,7 +4,59 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Principal {
-    Queue<Pessoa> fila = new LinkedList<Pessoa>();
+    public static void main(String[] args) {
 
-    Pessoa p1 = new Pessoa();
+        // Criando uma Queue
+        Queue<Pessoa> fila = new LinkedList<Pessoa>();
+
+        // Adionando os dados das Pessoas 
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Maria");
+        p1.setTel("552478765");
+        p1.setEmail("maria@gmail.com");
+
+        Pessoa p2 = new Pessoa();
+        p2.setNome("Joao");
+        p2.setTel("552485646");
+        p2.setEmail("joao@gmail.com");
+
+        Pessoa p3 = new Pessoa();
+        p3.setNome("Pedro");
+        p3.setTel("6698722");
+        p3.setEmail("pedro@gmail.com");
+
+        fila.add(p1);
+        fila.add(p2);
+        fila.add(p3);
+
+        //  Exibindo os dados das Pessoas 
+        for(Pessoa i : fila){
+            System.out.println("Nome: " + i.getNome() + "\n" + "Telefone: " + i.getTel() + "\n" + "Email: " + i.getEmail() + "\n");
+        }
+
+        // Removendo a primeira Pessoa 
+        System.out.println("-----------------------------------------------------------------------");
+
+        System.out.println("REMOVENDO A PRIMEIRA PESSOA");
+        fila.remove(p1);
+        for(Pessoa i : fila){
+            System.out.println("Nome: " + i.getNome() + "\n" + "Telefone: " + i.getTel() + "\n" + "Email: " + i.getEmail() + "\n");
+        }
+
+        // Adicionado uma nova Pessoa 
+        System.out.println("-----------------------------------------------------------------------");
+        
+        Pessoa p4 = new Pessoa();
+        p4.setNome("Sarah");
+        p4.setTel("998745226");
+        p4.setEmail("sarah@gmail.com");
+
+        fila.add(p4);
+
+        System.out.println("ADICIONANDO UMA NOVA PESSOA");
+        for(Pessoa i : fila){
+            System.out.println("Nome: " + i.getNome() + "\n" + "Telefone: " + i.getTel() + "\n" + "Email: " + i.getEmail() + "\n");
+        }
+    }
+
 }
