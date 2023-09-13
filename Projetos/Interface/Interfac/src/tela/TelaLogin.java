dbb91c8746338ee19faa5be12400b6fe43b3314c
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-09-13 08:48:36 -0300

    java

---
+++ Projetos/Interface/Interfac/src/tela/TelaLogin.java
@@ -0,0 +1,104 @@
+package tela;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaLogin {
+
+	// Variáveis 	
+	private JFrame frame;
+	private JTextField input1;
+	private JTextField input2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin window = new TelaLogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		// Tela / Janela 		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 455, 356);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		// Texto " Login" 		
+		JLabel texto1 = new JLabel("Login:");
+		texto1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		texto1.setBounds(72, 115, 46, 25);
+		frame.getContentPane().add(texto1);
+		
+		// Input do login		
+		input1 = new JTextField();
+		input1.setBounds(124, 113, 186, 31);
+		frame.getContentPane().add(input1);
+		input1.setColumns(10);
+		
+		// Texto " Senha "		
+		JLabel texto2 = new JLabel("Senha: ");
+		texto2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		texto2.setBounds(72, 186, 46, 14);
+		frame.getContentPane().add(texto2);
+		
+		// Input da senha 		
+		input2 = new JTextField();
+		input2.setColumns(10);
+		input2.setBounds(124, 179, 186, 31);
+		frame.getContentPane().add(input2);
+		
+		// Título		
+		JLabel titulo = new JLabel("Login");
+		titulo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		titulo.setBounds(193, 47, 40, 25);
+		frame.getContentPane().add(titulo);
+		
+		// Botão " Entrar "		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(checkLogin(input1.getText(),input2.getText())) {
+					JOptionPane.showMessageDialog(null, "Bem vindo ao sistema!");
+				}else {
+					JOptionPane.showMessageDialog(null,"Dados inválidos!","Mensagem",JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		});
+		btnEntrar.setBounds(169, 241, 89, 31);
+		frame.getContentPane().add(btnEntrar);
+	}
+	
+	// Método de verificação 	
+	public boolean checkLogin(String login, String senha) {
+		return login.equals("Usuário") && senha.equals("123");
+	}
+}
---
+++ Projetos/Interface/Interfac/src/tela/exibindo_nome.java
@@ -0,0 +1,98 @@
+package tela;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class exibindo_nome {
+
+	private JFrame frame;
+	private JTextField input;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					exibindo_nome window = new exibindo_nome();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public exibindo_nome() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	// Janela 	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setSize(450, 350);
+		
+		// Título da tela 		
+		JLabel titulo = new JLabel("Exibindo Nome");
+		titulo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		titulo.setBounds(165, 35, 121, 14);
+		frame.getContentPane().add(titulo);
+		
+		// Texto 1		
+		JLabel texto1 = new JLabel("Digite o seu nome: ");
+		texto1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		texto1.setBounds(56, 102, 112, 22);
+		frame.getContentPane().add(texto1);
+		
+		// Input 		
+		input = new JTextField();
+		input.setBounds(175, 104, 156, 20);
+		frame.getContentPane().add(input);
+		input.setColumns(10);
+		
+		// Botão Mostrar
+		// Criando a  função "Mostrar"  
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Criando uma tela para exibir o nome 			
+				JOptionPane.showMessageDialog(null,"Nome: " + input.getText());
+			}
+
+			}
+		);		
+		btnMostrar.setBounds(165, 163, 89, 23);
+		frame.getContentPane().add(btnMostrar);
+		
+		// Botão Sair 	
+		// Criando a função " Sair "		
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Comando que fecha a Janela 				
+				System.exit(0);
+			}
+		});
+		btnSair.setBounds(165, 209, 89, 23);
+		frame.getContentPane().add(btnSair);
+	}
+}
